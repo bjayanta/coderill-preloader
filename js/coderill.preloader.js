bd6b0cf739e741d4a38601592558506e7b74846e
Jayanta Biswas <bjayanta.me@gmail.com>
2017-07-20 13:21:47 +0600

    add some options

--- js/coderill.preloader.js
+++ js/coderill.preloader.js
@@ -4,20 +4,31 @@
 
 		// Establish our default settings
 		var settings = $.extend({
-			background		: '#F3F3F3',
-			foreground		: '#3498DB',
-			complete		: null
+			radius					: 50,
+			spinnerBackground		: '#F3F3F3',
+			spinnerForeground		: '#3498DB',
+			spinnerBorderWidth		: 5,
+			mainBackground			: 'rgba(40, 40, 40, 0.65)',
+			complete				: null
 		}, options);
 
 		return this.each(function() {
 			var self = this,
 				spinner = $(this).find('div.loader');
 
+			// set main container style
+			$(self).css('background', settings.mainBackground);
+
+			// set spinner style
 			spinner.css({
-				borderColor		: settings.background,
-				borderTopColor	: settings.foreground
+				width			: (settings.radius * 2) + 'px',
+				height			: (settings.radius * 2) + 'px',
+				borderWidth		: settings.spinnerBorderWidth + 'px',
+				borderColor		: settings.spinnerBackground,
+				borderTopColor	: settings.spinnerForeground
 			});
 
+			/*
 			$(window).on('load', function() {
 				$.when($(self).remove()).then(function() {
 					// callback method
@@ -26,6 +37,7 @@
 					}
 				});
 			});
+			*/
 		});
 
 	}
